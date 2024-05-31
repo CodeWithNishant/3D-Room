7e8477931b80857cf32cdf083142e9f8e46010db
Nishant Nagar <nishantnagar54321@gmail.com>
2024-05-31 19:51:21 +0530

    deleted every other file

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-// vite.config.js
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  base: "./", // Ensures correct path resolution for assets when deployed
-  build: {
-    outDir: "dist", // Output directory for build
-  },
-});
